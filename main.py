43ccbb8fb39e92d4662fad35ba23219efb202b45
TreySchneider95 <tschneid95@gmail.com>
2024-03-08 16:01:22 -0600

    moved span styles to front of the merge, added blacklist for the span styles, and added checks for strong or itallic tags to add them back in style

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import sys
 
-BLACK_LIST = ('mso-border-alt:', 'word-break:keep-all')
+BLACK_LIST = ('mso-border-alt:', 'word-break:keep-all',)
+SPAN_BLACK_LIST = ('display:inline-block')
 
 # Open the file provided and cast it to beautiful soup using the html parser
 with open(sys.argv[1], 'r+') as content:
@@ -19,15 +20,26 @@ def get_html():
     span_anchors = [anchor for anchor in anchors if anchor.find_all('span')]
     for anchor in span_anchors:
         anchors_spans = anchor.find_all('span')
+        for s in anchors_spans:
+            # Remove the blacklist styles from the spans
+            s['style'] =  ';'.join([x for x in s['style'].split(';') if not x.startswith(SPAN_BLACK_LIST)])
         style = ''.join([span['style'] for span in anchors_spans])
         text = anchors_spans[0].get_text()
         anchor.string = text
-        anchor['style'] = anchor['style'] + style
+        anchor['style'] = style + anchor['style']
         styles = [x for x in anchor['style'].split(';') if not x.startswith(BLACK_LIST)]
+        # Check for strong tag in the span and apply it with style
+        if [span for span in anchors_spans if span.find_all('strong')]:
+            styles.insert(-2, 'font-weight:bold')
+            print(styles)
+        # Check for I or EM tag in span and apply it with style
+        elif [span for span in anchors_spans if span.find_all('i') or span.find_all('em')]:
+            styles.insert(-2, 'font-style:italic')
         styles = ';'.join(styles)
         anchor['style'] = styles
         for span in anchors_spans:
             span.decompose()
+        
 
 get_html()
 
